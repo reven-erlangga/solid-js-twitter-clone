a2918761ca32ad8b2412763f28192a37a73d3329
reven-erlangga <reven.erlangga@gmail.com>
2023-05-20 01:48:53 +0700

    add: adding screens

---
+++ src/screens/Login.tsx
@@ -0,0 +1,7 @@
+import MainLayout from "../components/layouts/Main";
+
+const LoginScreen = () => {
+  return <MainLayout>Hello I am LoginScreen</MainLayout>;
+};
+
+export default LoginScreen;
---
+++ src/screens/Register.tsx
@@ -0,0 +1,7 @@
+import MainLayout from "../components/layouts/Main";
+
+const RegisterScreen = () => {
+  return <MainLayout>Hello I am RegisterScreen</MainLayout>;
+};
+
+export default RegisterScreen;
